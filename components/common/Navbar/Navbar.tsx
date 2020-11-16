3ff5679f97480af5f943fabd30d7dd6d46c47ec4
Belen Curcio <curciobelen@gmail.com>
2020-11-16 10:17:47 -0300

    Changes TS

--- components/common/Navbar/Navbar.tsx
+++ components/common/Navbar/Navbar.tsx
@@ -23,7 +23,7 @@ interface Link {
   url: string
 }
 
-const Navbar: FC<Props> = ({ data: { links = [] } }) => {
+const Navbar: FC<Props> = ({ data }) => {
   const [hasScrolled, setHasScrolled] = useState(false)
 
   const handleScroll = () => {
@@ -96,11 +96,12 @@ const Navbar: FC<Props> = ({ data: { links = [] } }) => {
       <div className="py-4 border-b border-gray-300">
         <Container>
           <nav className="hidden lg:flex flex-row space-x-6 items-center justify-center font-semibold tracking-widest text-sm uppercase">
-            {links.map(({ link }) => (
+            {data?.links.map(({ link }) => (
               <a className="cursor-pointer hover:text-gray-600" href={link.url}>
                 {link.title}
               </a>
             ))}
+
             <a className="cursor-pointer hover:text-gray-600">Search</a>
             <UserNav />
           </nav>
